2ca3c548f662d547ab0647999e4108b828156471
AM LAHMAR <abdelmalek.lahmar@gmail.com>
2026-05-11 17:15:11 +0100

    refactor: overhaul connection status script to include hotspot support and persistent display styling

--- polybar/.config/polybar/scripts/connection_status.sh
+++ polybar/.config/polybar/scripts/connection_status.sh
@@ -3,81 +3,82 @@
 # Configuration
 GLOBAL_STATE_FILE="/tmp/polybar_active_module"
 
+# Colors
+COLOR_ACTIVE="%{F#F0C674}"   # Primary Yellow
+COLOR_BT_ACTIVE="%{F#61afef}" # Blue for BT
+COLOR_DIM="%{F#707880}"      # Grey for inactive
+COLOR_OFFLINE="%{F#A54242}"  # Red
+COLOR_RESET="%{F-}"
+
 # Icons
 ICON_WIFI=""
 ICON_ETH="󰈀"
 ICON_BT=""
-ICON_OFFLINE="󰖪"
+ICON_HOTSPOT="󰖇"
 
 while true; do
-    # 1. Get WiFi Info
+    # 1. WiFi Status
     WIFI_SSID=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d: -f2)
     if [ -n "$WIFI_SSID" ]; then
-        WIFI_ICON="%{F#F0C674}%{T4}${ICON_WIFI}%{T-}%{F-}"
-        WIFI_TEXT="$WIFI_SSID"
+        CLR_WIFI="$COLOR_ACTIVE"
+        WIFI_INFO="$WIFI_SSID"
     else
-        WIFI_ICON=""
-        WIFI_TEXT=""
+        CLR_WIFI="$COLOR_DIM"
+        WIFI_INFO="Disconnected"
     fi
 
-    # 2. Get Ethernet Info
+    # 2. Ethernet Status
     ETH_STATE=$(nmcli device status | grep 'ethernet' | awk '{print $3}')
     if [ "$ETH_STATE" == "connected" ]; then
-        ETH_ICON="%{F#F0C674}%{T4}${ICON_ETH}%{T-}%{F-}"
-        ETH_TEXT="Wired"
+        CLR_ETH="$COLOR_ACTIVE"
+        ETH_INFO="Connected"
     else
-        ETH_ICON=""
-        ETH_TEXT=""
+        CLR_ETH="$COLOR_DIM"
+        ETH_INFO="Disconnected"
     fi
 
-    # 3. Get Bluetooth Info
+    # 3. Hotspot Status
+    HOTSPOT_ACTIVE=$(nmcli -t -f active,mode dev wifi | grep '^yes:ap' | wc -l)
+    if [ "$HOTSPOT_ACTIVE" -gt 0 ]; then
+        CLR_HOTSPOT="$COLOR_ACTIVE"
+        HOTSPOT_INFO="Active"
+    else
+        CLR_HOTSPOT="$COLOR_DIM"
+        HOTSPOT_INFO="Off"
+    fi
+
+    # 4. Bluetooth Status
     BT_POWERED=$(bluetoothctl show | grep "Powered: yes" | wc -l)
     BT_CONNECTED_NAME=$(bluetoothctl devices Connected | cut -d ' ' -f 3-)
     
     if [ "$BT_POWERED" -gt 0 ]; then
         if [ -n "$BT_CONNECTED_NAME" ]; then
-            BT_ICON="%{F#61afef}%{T4}${ICON_BT}%{T-}%{F-}"
-            BT_TEXT="$BT_CONNECTED_NAME"
+            CLR_BT="$COLOR_BT_ACTIVE"
+            BT_INFO="$BT_CONNECTED_NAME"
         else
-            BT_ICON="%{F#707880}%{T4}${ICON_BT}%{T-}%{F-}"
-            BT_TEXT="On"
+            CLR_BT="$COLOR_ACTIVE"
+            BT_INFO="On (Idle)"
         fi
     else
-        BT_ICON=""
-        BT_TEXT=""
+        CLR_BT="$COLOR_DIM"
+        BT_INFO="Off"
     fi
 
-    # 4. Check Global State
+    # 5. Build Output
     ACTIVE_MODULE=$(cat "$GLOBAL_STATE_FILE" 2>/dev/null || echo "none")
 
-    if [ -z "$WIFI_ICON" ] && [ -z "$ETH_ICON" ]; then
-        ICON_STATUS="%{F#A54242}%{T4}${ICON_OFFLINE}%{T-}%{F-}"
-        TEXT_STATUS="Offline"
-    else
-        ICON_STATUS=""
-        TEXT_STATUS=""
-    fi
+    # Icons with tags
+    W_ICON="%{T4}${CLR_WIFI}${ICON_WIFI}${COLOR_RESET}%{T-}"
+    E_ICON="%{T4}${CLR_ETH}${ICON_ETH}${COLOR_RESET}%{T-}"
+    H_ICON="%{T4}${CLR_HOTSPOT}${ICON_HOTSPOT}${COLOR_RESET}%{T-}"
+    B_ICON="%{T4}${CLR_BT}${ICON_BT}${COLOR_RESET}%{T-}"
 
     if [ "$ACTIVE_MODULE" == "connection" ]; then
         # Expanded View
-        OUTPUT=""
-        [ -n "$WIFI_ICON" ] && OUTPUT="${WIFI_ICON} ${WIFI_TEXT}"
-        [ -n "$ETH_ICON" ] && [ -n "$OUTPUT" ] && OUTPUT="${OUTPUT}  "
-        [ -n "$ETH_ICON" ] && OUTPUT="${OUTPUT}${ETH_ICON} ${ETH_TEXT}"
-        [ -n "$BT_ICON" ] && [ -n "$OUTPUT" ] && OUTPUT="${OUTPUT}  "
-        [ -n "$BT_ICON" ] && OUTPUT="${OUTPUT}${BT_ICON} ${BT_TEXT}"
-        [ -n "$ICON_STATUS" ] && OUTPUT="${ICON_STATUS} ${TEXT_STATUS}"
-        echo "$OUTPUT"
+        echo "${W_ICON} ${WIFI_INFO}  ${E_ICON} ${ETH_INFO}  ${H_ICON} ${HOTSPOT_INFO}  ${B_ICON} ${BT_INFO}"
     else
-        # Collapsed View
-        OUTPUT=""
-        [ -n "$WIFI_ICON" ] && OUTPUT="${WIFI_ICON}"
-        [ -n "$ETH_ICON" ] && [ -n "$OUTPUT" ] && OUTPUT="${OUTPUT}  "
-        [ -n "$ETH_ICON" ] && OUTPUT="${OUTPUT}${ETH_ICON}"
-        [ -n "$BT_ICON" ] && [ -n "$OUTPUT" ] && OUTPUT="${OUTPUT}  "
-        [ -n "$BT_ICON" ] && OUTPUT="${OUTPUT}${BT_ICON}"
-        [ -n "$ICON_STATUS" ] && OUTPUT="${ICON_STATUS}"
-        echo "$OUTPUT"
+        # Collapsed View (all icons)
+        echo "${W_ICON} ${E_ICON} ${H_ICON} ${B_ICON}"
     fi
 
     sleep 2
